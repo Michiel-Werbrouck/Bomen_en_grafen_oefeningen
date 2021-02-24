b647921a317f3df66509be0c960339a80a62595a
Michiel Werbrouck <michiel.werbrouck@student.ucll.be>
2021-02-24 16:24:05 +0100

    BinaryTree

--- week02_bomen/src/ui/BinaryTreeDriver.java
+++ week02_bomen/src/ui/BinaryTreeDriver.java
@@ -2,10 +2,30 @@ package ui;
 
 import domain.BinaryTree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BinaryTreeDriver {
 
 	public static void main(String[] args) {
-		BinaryTree<String> nodeD = new BinaryTree<>("D");
+		//2.1 Boom
+		BinaryTree<String> nodeC = new BinaryTree<>("C");
+		BinaryTree<String> nodeE = new BinaryTree<>("E");
+		BinaryTree<String> nodeH = new BinaryTree<>("H");
+		BinaryTree<String> nodeA = new BinaryTree<>("A");
+
+		BinaryTree<String> nodeD = new BinaryTree<>("D", nodeC, nodeE);
+		BinaryTree<String> nodeI = new BinaryTree<>("I",nodeH,null);
+
+		BinaryTree<String> nodeB = new BinaryTree<>("B", nodeA, nodeD);
+		BinaryTree<String> nodeG = new BinaryTree<>("G",null,nodeI);
+
+		BinaryTree<String> root = new BinaryTree<>("F", nodeB, nodeG);
+
+		System.out.println(root.contains("A"));
+
+		//Oude Boom
+		/*BinaryTree<String> nodeD = new BinaryTree<>("D");
 		BinaryTree<String> nodeF = new BinaryTree<>("F");
 		BinaryTree<String> nodeB = new BinaryTree<>("B");
 		BinaryTree<String> nodeH = new BinaryTree<>("H");
@@ -19,7 +39,7 @@ public class BinaryTreeDriver {
 		
 		// boom heeft root C en heeft links A en rechts G
 		BinaryTree<String> boom = new BinaryTree<>("C",nodeA, nodeG);
-		boom.printPreorder();
+		boom.printPreorder();*/
 	}
 
 }

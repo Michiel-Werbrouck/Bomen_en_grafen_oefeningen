dcdce95412c6dde137e529c3c1fbece9715bd19f
= <michiel.werbrouck@student.ucll.be>
2021-06-09 18:28:04 +0200

    Verdieping Oef 10.9

--- week10_Grafen_verdieping/src/domain/Graph.java
+++ week10_Grafen_verdieping/src/domain/Graph.java
@@ -1,8 +1,33 @@
 package domain;
 
-public class Graph {
+import java.util.ArrayList;
 
-    public Integer isVergeetPut(int[][] verbindingsMatrix) {
+public class Graph {
+    private final int[][] verbindingsMatrix;
+
+    public Graph(int[][] matrix) {
+        if (!isGeldigeVerbindingsMatrix(matrix))
+            throw new IllegalArgumentException("No valid verbindingsmatrix");
+
+        this.verbindingsMatrix = matrix.clone();
+    }
+
+    private boolean isGeldigeVerbindingsMatrix(int[][] matrix) {
+        if (matrix == null || matrix.length != matrix[0].length)
+            return false;
+
+        for (int i = 0; i < matrix.length; i++)
+            if (matrix[i][i] != 0)
+                return false;
+
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix.length; j++)
+                if (matrix[i][j] != 0 && matrix[i][j] != 1)
+                    return false;
+        return true;
+    }
+
+    public Integer isVergeetPut() {
         for (int i = 0; i < verbindingsMatrix.length; i++) {
             boolean vergeetPutGevonden = true;
             for (int j = 0; j < verbindingsMatrix.length; j++) {
@@ -15,6 +40,50 @@ public class Graph {
         return null;
     }
 
+    private int getAantalKnopen() {
+        return verbindingsMatrix.length;
+    }
 
+
+    public ArrayList<Double> verdeel(int i, double bedrag) {
+        ArrayList<Double> verdeelt = new ArrayList<>();
+        ArrayList<Integer> delers = new ArrayList<>();
+        for (int j = 0; j < this.getAantalKnopen(); j++) {
+            if (i == j) {
+                verdeelt.add(bedrag);
+                delers.add(i);
+            } else {
+                verdeelt.add(0.0);
+            }
+        }
+        return verdeelHelper(verdeelt, delers);
+    }
+
+    private ArrayList<Double> verdeelHelper(ArrayList<Double> verdeelt, ArrayList<Integer> delers) {
+        if (delers.size() == 0) {
+            return verdeelt;
+        } else {
+            ArrayList<Integer> nieuweDelers = new ArrayList<>();
+            for (int deler : delers) {
+                int[] kinderen = this.verbindingsMatrix[deler];
+                ArrayList<Integer> deelMetKinderen = new ArrayList<>();
+                for (int j = 0; j < kinderen.length; j++) {
+                    if (kinderen[j] == 1 && verdeelt.get(j) == 0.0) {
+                        deelMetKinderen.add(j);
+                    }
+                }
+                if (deelMetKinderen.size() != 0) {
+                    double verdeelValue = verdeelt.get(deler) / 2;
+                    verdeelt.set(deler, verdeelValue);
+                    verdeelValue /= deelMetKinderen.size();
+                    for (int kind : deelMetKinderen) {
+                        verdeelt.set(kind, verdeelValue);
+                        if (!nieuweDelers.contains(kind)) nieuweDelers.add(kind);
+                    }
+                }
+            }
+            return verdeelHelper(verdeelt, nieuweDelers);
+        }
+    }
 
 }
--- week10_Grafen_verdieping/src/ui/graphDriver.java
+++ week10_Grafen_verdieping/src/ui/graphDriver.java
@@ -4,8 +4,8 @@ import domain.Graph;
 
 public class graphDriver {
     public static void main(String[] args) {
-        int[][] testVerbindingsMatrix = {{0,1,1,0,0},{1,0,1,1,1},{0,0,1,0,0},{0,1,0,0,1},{0,0,0,0,0}};
-        Graph graph = new Graph();
-        System.out.println(graph.isVergeetPut(testVerbindingsMatrix));
+        int[][] testVerbindingsMatrix = {{0,1,1,0},{0,0,0,1},{0,0,0,1},{1,0,0,0}};
+        Graph graph = new Graph(testVerbindingsMatrix);
+        System.out.println(graph.verdeel(0, 50.0));
     }
 }
